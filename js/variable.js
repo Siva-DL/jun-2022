db337b1db86d3ae083bbc05c030b5c85b51cf2ae
Siva-DL <sivaprasad.digitallync@gmail.com>
2022-07-04 09:04:22 +0530

    JS: DataTypes and Variables and Tasks

---
+++ js/variable.js
@@ -0,0 +1,33 @@
+var _name = "siva"; // string
+var contact = Infinity; // numbers | Infinity
+var email = "asdas@asdasdc.com"; // string
+var isValid = true; // true / false: boolean
+
+var un; // undefined
+var nu = null; // not: "", not: 0 // null
+
+console.log(un);
+
+un = "123";
+
+console.log(_name);
+
+// Primitive - DataTypes (literals): string, numbers, boolean, null and Undefined:
+// Non-Primitive - DataTypes: Object(array) and functions:
+
+// phone =  iphone
+// price: $123123
+// color: black/red/
+// dimensions: 30x30
+
+// key: value
+
+var Obj = {
+  price: 1000,
+  color: "red",
+  "product name": "iphone-x",
+};
+
+// Object.dot, Object['key']
+
+console.log(Obj["color"]);
